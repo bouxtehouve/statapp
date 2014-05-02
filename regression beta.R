b0799f3b4a9515129bc16a222e78e05ba3b9bcd1
yannlefeuvre <yannlefeuvreyl@hotmail.fr>
2014-05-02 20:09:31 +0200

    sharpe ic
    intervalle de confiance pour le ratio de sharpe

--- regression beta.R
+++ regression beta.R
@@ -3,14 +3,14 @@ source("C:/Users/Yann/Documents/GitHub/statapp/black_litterman_code_kevin.r")
 
 p=t(fopt2(var(rdt_j_a),colMeans(rdt_j_a),0.012))
 rdt_pf=t(p%*%t(rdt_j_a))
-
-lm.beta=lm(rdt_pf ~ rdt_j[,"cac40"])
+rdt_reg=rdt_j[,"cac40"]-mean(rdt_j[,"oat"])
+lm.beta=lm(rdt_pf ~ rdt_reg )
 
 summary(lm.beta)
 library(xtable)
 print(xtable(summary(lm.beta)), type="latex", file="D:/cours/2A/Statap/Mémoire/summary_beta.tex")
 
-plot(rdt_j[,"cac40"],rdt_pf,xlab="Rendement du marché",ylab="Rendement du portefeuille",main="Régression linéaire du rendement du portefeuille sur celui du marché")
+plot(rdt_j[,"cac40"],rdt_pf,xlab="R(marché)-r(sans risque)",ylab="R(portefeuille)",main="Régression du MEDAF")
 abline(lm.beta,col='red')
 
 # Tracés sur les résidus
